c504ca798b9c145a94a1e8b8fe97ce36aae405e1
Matt Cosentino <matt@learnedmedia.com>
2018-04-06 10:36:36 -0400

    added a backup and deploiy button

--- popup.js
+++ popup.js
@@ -1,21 +1,28 @@
 function rachelle() {
 	chrome.tabs.executeScript({
 		code: 'document.getElementById("comment-textarea").value = "@Rachelle Garaffa - changes made";jQuery(".btn-post").click();'
-	}); 
+	});
 }
 
 function zach() {
 	chrome.tabs.executeScript({
 		code: 'document.getElementById("comment-textarea").value = "@Zach Gordon - changes made";jQuery(".btn-post").click();'
-	}); 
+	});
 }
 
 function working() {
 	chrome.tabs.executeScript({
 		code: 'document.getElementById("comment-textarea").value = "working here";jQuery(".btn-post").click();'
-	}); 
+	});
+}
+
+function backUpDeploy() {
+	chrome.tabs.executeScript({
+		code: 'document.getElementById("comment-textarea").value = "backing up and deploying";jQuery(".btn-post").click();'
+	});
 }
 
 document.getElementById('rachelle').addEventListener('click', rachelle);
 document.getElementById('zach').addEventListener('click', zach);
 document.getElementById('working').addEventListener('click', working);
+document.getElementById('b-u-dep').addEventListener('click', backUpDeploy);
